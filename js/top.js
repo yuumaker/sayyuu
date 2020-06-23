32e5237033fe471ce8b08c6b46460510f2e9dacd
yuumaker <48975518+yuumaker@users.noreply.github.com>
2020-06-24 01:27:34 +0800

    Add files via upload

---
+++ js/top.js
@@ -0,0 +1,7 @@
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
